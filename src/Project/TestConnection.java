562087ef098cc0da4ff325cd7684f8a552c4bdbc
YasinUottawa <yasinelmi100@gmail.com>
2022-03-07 17:44:19 -0500

    set up connection to database

--- src/Project/TestConnection.java
+++ src/Project/TestConnection.java
@@ -3,9 +3,9 @@ package Project;
 import java.sql.*;
 
 public class TestConnection {
-    static final String DB_URL = "";
-    static final String USERNAME = "";
-    static final String PASSWORD = "";
+    static final String DB_URL = "us-cdbr-east-05.cleardb.net";
+    static final String USERNAME = "b559517d77ae6e";
+    static final String PASSWORD = "74499144";
     static final String QUERY = "";
 
     public static void main(String[] args){
